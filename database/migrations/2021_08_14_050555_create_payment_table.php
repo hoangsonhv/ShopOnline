851ac8b9a828ecbaa2640a029c6cd6d0a068c545
GiaThinh1711 <thinhdau1711@gmail.com>
2021-08-15 12:38:05 +0700

    ff

---
+++ database/migrations/2021_08_14_050555_create_payment_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payment', function (Blueprint $table) {
+            $table->id();
+            $table->integer('transaction_code')->nullable();
+            $table->float("money")->nullable();
+            $table->string("note")->nullable();
+            $table->string("respone_code")->nullable();
+            $table->string("code_vnpay")->nullable();
+            $table->string("code_bank")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payment');
+    }
+}
