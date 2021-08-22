3aad03d3dbf297bf3b1c53f44ced3af521d842eb
GiaThinh1711 <thinhdau1711@gmail.com>
2021-08-22 22:06:01 +0700

    heroku de lam phan chat

--- resources/views/web/components/scripts.blade.php
+++ resources/views/web/components/scripts.blade.php
@@ -15,7 +15,26 @@
 /*-------------------------------
  Price Slider Active
 --------------------------------*/
+<script>
+    var chatbox = document.getElementById('fb-customer-chat');
+    chatbox.setAttribute("page_id", "100558045692227");
+    chatbox.setAttribute("attribution", "biz_inbox");
 
+    window.fbAsyncInit = function() {
+        FB.init({
+            xfbml            : true,
+            version          : 'v11.0'
+        });
+    };
+
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = '{{url("https://connect.facebook.net/en_GB/sdk/xfbml.customerchat.js")}}';
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+</script>
 <script>
     var btnContainer = document.getElementById("active_button");
 
@@ -68,25 +87,5 @@
 </script>
 
 
-<script>
-    var chatbox = document.getElementById('fb-customer-chat');
-    chatbox.setAttribute("page_id", "100558045692227");
-    chatbox.setAttribute("attribution", "biz_inbox");
-
-    window.fbAsyncInit = function() {
-        FB.init({
-            xfbml            : true,
-            version          : 'v11.0'
-        });
-    };
-
-    (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = '{{url("https://connect.facebook.net/en_GB/sdk/xfbml.customerchat.js")}}';
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-</script>
 
 
--- resources/views/web/layout.blade.php
+++ resources/views/web/layout.blade.php
@@ -29,8 +29,7 @@
 <div id="fb-root"></div>
 
 <!-- Your Plugin chat code -->
-<div id="fb-customer-chat" class="fb-customerchat">
-</div>
+<div id="fb-customer-chat" class="fb-customerchat"></div>
 </body>
 
 </html>
