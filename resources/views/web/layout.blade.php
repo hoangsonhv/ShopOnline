37306a5bae79b30fd1ad205711bcaaf06b71da32
GiaThinh1711 <thinhdau1711@gmail.com>
2021-08-22 21:52:12 +0700

    heroku de lam phan chat

--- resources/views/web/layout.blade.php
+++ resources/views/web/layout.blade.php
@@ -4,33 +4,6 @@
 @include("web.components.head")
 
 <body>
-<!-- Messenger Plugin chat Code -->
-<div id="fb-root"></div>
-
-<!-- Your Plugin chat code -->
-<div id="fb-customer-chat" class="fb-customerchat">
-</div>
-
-<script>
-    var chatbox = document.getElementById('fb-customer-chat');
-    chatbox.setAttribute("page_id", "100558045692227");
-    chatbox.setAttribute("attribution", "biz_inbox");
-
-    window.fbAsyncInit = function() {
-        FB.init({
-            xfbml            : true,
-            version          : 'v11.0'
-        });
-    };
-
-    (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/en_GB/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-</script>
 <!-- Body main wrapper start -->
 <div class="wrapper">
     <!-- Start Header Style -->
@@ -52,7 +25,6 @@
 
 <!-- jquery latest version -->
 @include("web.components.scripts")
-</body>
 <!-- Messenger Plugin chat Code -->
 <div id="fb-root"></div>
 
@@ -80,4 +52,6 @@
         fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));
 </script>
+</body>
+
 </html>
