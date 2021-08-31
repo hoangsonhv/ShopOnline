b5aa345132327344ea6be07526a60b004520db50
hoangsonhv <70937660+hoangsonhv@users.noreply.github.com>
2021-08-31 10:22:32 +0700

    31/08 thong ke admin/upload

--- app/Http/Controllers/Web/WebController.php
+++ app/Http/Controllers/Web/WebController.php
@@ -557,8 +557,8 @@ class WebController extends Controller
     public function searchItem(Request $request){
         $search = $request->input('search');
         $products = Product::with(['category','brand'])->where('name','LIKE',"%{$search}%")
-            ->orWhere('unit_price','LIKE',"$search")
-            ->orWhere('promotion_price','LIKE',"$search")
+            ->orWhere('unit_price',"$search")
+            ->orWhere('promotion_price',"$search")
             ->orderBy('unit_price',"DESC")
             ->paginate(9);
 //        if ($request->price){
